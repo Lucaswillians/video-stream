55770eff4136947681bbc2b76c0c05fc2a711684
Cristian Prochnow <cristianprochnow@gmail.com>
2024-02-24 13:36:12 -0300

    ajustando estilos do header

--- src/pages/BasePage/index.js
+++ src/pages/BasePage/index.js
@@ -3,11 +3,12 @@ import Footer from "components/Footer";
 import Header from "components/Header";
 import FavoritesProvider from "contexts/Favorites";
 import { Outlet } from "react-router-dom";
+import styles from "./BasePage.module.css";
 
 export default function BasePage ()
 {
   return (
-    <main>
+    <main className={styles.container}>
        <Header/>
             <FavoritesProvider>
                  <Container>
