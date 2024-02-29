bb4f79c93da57ec34c09d4ef652320e2eae78d66
Cristian Prochnow <cristianprochnow@gmail.com>
2024-02-29 16:01:24 +0000

    ajustando layout da tela de favoritos também

--- src/pages/Favorites/index.js
+++ src/pages/Favorites/index.js
@@ -3,18 +3,21 @@ import styles from "./Favorites.module.css";
 import Title from "components/Title";
 import Card from "components/Card";
 import { useFavoriteContext } from "contexts/Favorites";
+import Wrapper from "components/Wrapper";
 
 export default function Favorites() {
   const { favorite } = useFavoriteContext();
   return (
-    <div>
+    <>
       <Banner image="favoritos" />
-      <Title> <h1> Meus favoritos </h1> </Title>
-      <section className={styles.container}>
-          {favorite.map((fav) => {
-            return <Card {...fav} key={fav.id}/>
-          })}
-      </section>
-    </div>
+      <Wrapper>
+        <Title> <h1> Meus favoritos </h1> </Title>
+        <section className={styles.container}>
+            {favorite.map((fav) => {
+              return <Card {...fav} key={fav.id}/>
+            })}
+        </section>
+      </Wrapper>
+    </>
   );
 }
